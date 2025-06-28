6a2a0b319f5d68e1d0cb2d0797e72895fa749e0f
PercyTuncar <percy.edgar.tuncar@gmail.com>
2025-06-28 03:24:57 -0400

    Actualizo mee.js con últimos cambios

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = (sock) => {
+module.exports = () => {
     const commands = new Map();
     const commandFolders = ['admin', 'economy', 'utility'];
 
@@ -9,7 +9,6 @@ module.exports = (sock) => {
         const commandFiles = fs.readdirSync(path.join(__dirname, '..', 'commands', folder)).filter(file => file.endsWith('.js'));
         for (const file of commandFiles) {
             const command = require(`../commands/${folder}/${file}`);
-            command.sock = sock; // Inyectar sock en cada comando
             commands.set(command.name, command);
         }
     }
