926f4db69d6fee85588361dda30abe3ef4901eb0
PercyTuncar <percy.edgar.tuncar@gmail.com>
2025-06-30 09:40:44 -0400

    se mejora las descripciones

--- config/database.js
+++ config/database.js
@@ -1,12 +1,9 @@
-
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
         });
         console.log('Conectado a MongoDB Atlas');
     } catch (error) {
