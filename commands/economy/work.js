31357bd5f327a92bc5045fc416d751b0a9e965d5
PercyTuncar <percy.edgar.tuncar@gmail.com>
2025-07-01 21:01:31 -0400

    se formatea el codigo con pretier

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -69,15 +69,16 @@ module.exports = {
       await user.save();
 
       // Mensaje principal del trabajo
-      let workResponse = `🔨════════════ 🔨
-*💼 ¡BUEN TRABAJO! 💼*
-🔨════════════ 🔨
+    let workResponse = `┌─────────────────────────┐
+│  💼 BUEN TRABAJO 💵  │
+└─────────────────────────┘
 
-👤 @${senderJid.split("@")[0]}
+👤 @${senderJid.split('@')[0]}
 🧹 *Puesto:* _${job.name}_
 > 🤫 *Detalle:* _${job.description}_
 💰 *Salario:* \`${currency} ${earnings.toLocaleString()}\`
 🌟 *XP:* \`\`\`+${xpGained}\`\`\``;
+
 
       // Obtener el mensaje de recordatorio de deuda
       const debtReminder = await getDebtReminderMessage(user);
