62710f5feb2cea952904e59ec2d323e62b087e35
PercyTuncar <percy.edgar.tuncar@gmail.com>
2025-07-03 18:59:55 -0400

    cambios refactorizacion del juego

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+
 let sock = null;
 
 function setSocket(socket) {
@@ -12,13 +13,3 @@ module.exports = {
     setSocket,
     getSocket,
 };
-
-// Manejar respuestas de juegos
-if (!isCommand) {
-  const jid = msg.key.participant || msg.key.remoteJid;
-  if (jid) {
-    await handleGameResponse(msg, jid, sock);
-  }
-}
-
-// Resto de los manejadores de eventos...
