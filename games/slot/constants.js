8b4ddb8194c887631de05684bf7211d21b2ab2cf
PercyTuncar <percy.edgar.tuncar@gmail.com>
2025-07-03 23:06:46 -0400

    se sube el pago para que todos ganen en slot

--- games/slot/constants.js
+++ games/slot/constants.js
@@ -1,13 +1,13 @@
 const SYMBOLS = [
-    { emoji: '🍒', name: 'Cereza', weight: 3000, payouts: { 3: 2, 2: 1 } },
-    { emoji: '🍋', name: 'Limón', weight: 3000, payouts: { 3: 2.5, 2: 1.2 } },
-    { emoji: '🍊', name: 'Naranja', weight: 2000, payouts: { 3: 3, 2: 1.5 } },
-    { emoji: '🍇', name: 'Uva', weight: 1000, payouts: { 3: 4, 2: 2 } },
-    { emoji: '🍉', name: 'Sandía', weight: 600, payouts: { 3: 6, 2: 3 } },
-    { emoji: '🍎', name: 'Manzana', weight: 300, payouts: { 3: 8, 2: 4 } },
-    { emoji: '💎', name: 'Diamante', weight: 80, payouts: { 3: 12, 2: 6 } },
-    { emoji: '👑', name: 'Corona', weight: 15, payouts: { 3: 20, 2: 10 } },
-    { emoji: '🎰', name: 'Jackpot', weight: 5, payouts: { 3: 50, 2: 25 } },
+    { emoji: '🍒', name: 'Cereza', weight: 3000, payouts: { 3: 3, 2: 1.5 } },
+    { emoji: '🍋', name: 'Limón', weight: 3000, payouts: { 3: 4, 2: 2 } },
+    { emoji: '🍊', name: 'Naranja', weight: 2000, payouts: { 3: 5, 2: 2.5 } },
+    { emoji: '🍇', name: 'Uva', weight: 1000, payouts: { 3: 8, 2: 3 } },
+    { emoji: '🍉', name: 'Sandía', weight: 600, payouts: { 3: 12, 2: 4 } },
+    { emoji: '🍎', name: 'Manzana', weight: 300, payouts: { 3: 18, 2: 5 } },
+    { emoji: '💎', name: 'Diamante', weight: 80, payouts: { 3: 25, 2: 8 } },
+    { emoji: '👑', name: 'Corona', weight: 15, payouts: { 3: 50, 2: 15 } },
+    { emoji: '🎰', name: 'Jackpot', weight: 5, payouts: { 3: 100, 2: 25 } },
 ];
 
 const MIN_BET = 10;
