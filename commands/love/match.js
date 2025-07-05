8c662015d5eac0344337628b0e1e605935fd9f00
PercyTuncar <percy.edgar.tuncar@gmail.com>
2025-07-05 02:35:40 -0400

    se agrega amor al grupo

--- commands/love/match.js
+++ commands/love/match.js
@@ -1,6 +1,6 @@
 const User = require('../../models/User');
 const { getSocket } = require('../../bot');
-const { getMentions } = require('../../utils/messageUtils'); // Asumiendo que tienes una utilidad para obtener menciones
+const { findOrCreateUser } = require('../../utils/userUtils');
 
 const ongoingMatches = new Map();
 
@@ -18,20 +18,27 @@ module.exports = {
 
         try {
             const groupMetadata = await sock.groupMetadata(from);
-            const participants = groupMetadata.participants.map(p => p.id);
+            const participants = groupMetadata.participants.map(p => ({ jid: p.id, name: p.subject || p.id.split('@')[0] }));
 
-            const users = await User.find({ jid: { $in: participants }, 'loveInfo.relationshipStatus': 'Soltero/a' });
+            const usersInRelationship = await User.find({ 'loveInfo.relationshipStatus': 'En una relación' }, 'jid');
+            const jidsInRelationship = usersInRelationship.map(u => u.jid);
 
-            if (users.length < 2) {
+            const singleParticipants = participants.filter(p => !jidsInRelationship.includes(p.jid));
+
+            if (singleParticipants.length < 2) {
                 return sock.sendMessage(from, { text: 'No hay suficientes solteros en el grupo para un match.' }, { quoted: message });
             }
 
-            const userA = users[Math.floor(Math.random() * users.length)];
-            let userB = users[Math.floor(Math.random() * users.length)];
+            const userA_info = singleParticipants[Math.floor(Math.random() * singleParticipants.length)];
+            let userB_info = singleParticipants[Math.floor(Math.random() * singleParticipants.length)];
 
-            while (userA.jid === userB.jid) {
-                userB = users[Math.floor(Math.random() * users.length)];
+            while (userA_info.jid === userB_info.jid) {
+                userB_info = singleParticipants[Math.floor(Math.random() * singleParticipants.length)];
             }
+
+            // Asegurarse que los usuarios existan en la DB
+            const userA = await findOrCreateUser(userA_info.jid, from, userA_info.name);
+            const userB = await findOrCreateUser(userB_info.jid, from, userB_info.name);
 
             const matchInfo = {
                 userA: { jid: userA.jid, name: userA.name, accepted: false },
